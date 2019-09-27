9bb749b0d1dbfa11103d96bb6a29e8c54bfe4288
carlos.schlyter <carlos.schlyter@nexxera.com>
2019-09-27 16:41:24 -0300

    Chapter 4 - Security

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package sia.tacocloud;
+package tacos;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TacoCloudApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }

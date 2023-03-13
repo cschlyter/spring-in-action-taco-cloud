26cb48380d1a80c39317a335a9d730f250d73b56
carlos.schlyter <carlos.schlyter@nexxera.com>
2023-03-13 18:51:07 -0300

    Chapter 4 - Security

---
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package tacos.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+}
